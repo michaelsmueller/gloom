b6401be7422e1aa6dd513cfb5026803630e46703
Michael Mueller <hello@michaelmueller.dev>
2020-12-21 14:29:25 +0100

    reorganization Auction contract to improve readability

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.3;
 
 import '@openzeppelin/upgrades/contracts/Initializable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-// import '@chainlink/contracts/src/v0.5/ChainlinkClient.sol';
 import './AuctionFactory.sol';
 import './Escrow.sol';
 
@@ -39,16 +38,34 @@ contract Auction is Initializable {
   address[] public bidderAddresses;
 
   event LogSellerDepositReceived(address indexed seller, uint256 sellerDeposit);
-  event LogBidderDepositReceived(address indexed bidder, uint256 bidderDeposit);
   event LogSellerDepositWithdrawn(address indexed seller, uint256 amount);
+
+  event LogBidderDepositReceived(address indexed bidder, uint256 bidderDeposit);
+  event LogBidderDepositWithdrawn(address indexed bidder, uint256 amount);
 
   event LogBidderInvited(address indexed bidder);
   event LogBidCommitted(address indexed bidder, bytes32 bidHash, uint256 bidCommitBlock);
   event LogBidRevealed(address indexed bidder, bytes32 bidHex, bytes32 salt);
+
   event LogSetWinner(address indexed bidder, uint256 bid);
 
   modifier onlySeller {
     require(msg.sender == seller, 'Sender not authorized');
+    _;
+  }
+
+  modifier onlyBidder {
+    require(isInvitedBidder(msg.sender), 'Sender not authorized');
+    _;
+  }
+
+  modifier onlySellerOrBidder {
+    require(msg.sender == seller || isInvitedBidder(msg.sender), 'Sender not authorized');
+    _;
+  }
+
+  modifier onlySellerOrWinner {
+    require(msg.sender == seller || msg.sender == winner, 'Sender not authorized');
     _;
   }
 
@@ -84,7 +101,6 @@ contract Auction is Initializable {
     uint256 _startDateTime,
     uint256 _endDateTime
   ) public initializer {
-    // Ownable.initialize(_seller);
     factory = msg.sender;
     seller = _seller;
     tokenAmount = _tokenAmount;
@@ -92,109 +108,78 @@ contract Auction is Initializable {
     startDateTime = _startDateTime;
     endDateTime = _endDateTime;
     phase = Phase.Setup;
-
-    // setPublicChainlinkToken();
-    // oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
-    // jobId = '29fa9aa13bf1468788b7cc4a500a45b8';
-    // oracleFee = 0.1 * 10 ** 18; // 0.1 LINK
   }
 
-  function getBalance() external view onlySeller returns (uint256) {
-    return address(this).balance;
-  }
-
-  function receiveSellerDeposit() external payable onlySeller inSetup {
-    // consider using initialize or other modifier to prevent seller from changing deposit
-    sellerDeposit = msg.value;
-    emit LogSellerDepositReceived(msg.sender, msg.value);
-  }
-
-  function withdrawSellerDeposit() external payable onlySeller inWithdraw {
-    // require(bothOk(), 'Escrow is not complete');
-    require(address(this).balance >= sellerDeposit, 'Insufficient balance');
-    // balance -= winningBid;
-    (bool success, ) = msg.sender.call.value(sellerDeposit)('');
-    require(success, 'Transfer failed');
-    emit LogSellerDepositWithdrawn(msg.sender, sellerDeposit);
-  }
-
-  function getPhase() external view returns (Phase) {
-    require(msg.sender == seller || isInvitedBidder(msg.sender), 'Sender not authorized');
-    return phase;
-  }
-
-  function getWinner() external view returns (address, uint256) {
-    require(msg.sender == seller || isInvitedBidder(msg.sender), 'Sender not authorized');
-    uint256 winningBid = uint256(bidders[winner].bidHex);
-    return (winner, winningBid);
-  }
-
-  function getEscrow() external view returns (Escrow) {
-    require(msg.sender == seller || msg.sender == winner, 'Sender not authorized');
-    return escrow;
-  }
+  // ALL PHASES OPEN
 
   function getDateTimes() external view returns (uint256, uint256) {
     return (startDateTime, endDateTime);
   }
 
-  function getAsset() external view returns (uint256, address) {
-    return (tokenAmount, tokenContractAddress);
+
+  // public function, triggered by bidder in frontend in Commit phase and internally in Reveal phase
+  function getSaltedHash(bytes32 data, bytes32 salt) public view returns (bytes32) {
+    return keccak256(abi.encodePacked(address(this), data, salt));
   }
 
-  function getBidders() external view returns (address[] memory) {
-    return bidderAddresses;
+
+  // ALL PHASES PRIVATE
+    
+  function isInvitedBidder(address bidderAddress) private view returns (bool) {
+    return bidders[bidderAddress].isInvited;
   }
 
-  function getBidderDeposit() external view returns (uint256) {
-    require(isInvitedBidder(msg.sender), 'Sender not authorized');
-    return bidderDeposit;
+
+
+  // ALL PHASES ONLY SELLER
+
+  function getBalance() external view onlySeller returns (uint256) {
+    return address(this).balance;
   }
 
   function getSellerDeposit() external view onlySeller returns (uint256) {
     return sellerDeposit;
   }
 
-  function isInvitedBidder(address bidderAddress) private view returns (bool) {
-    return bidders[bidderAddress].isInvited;
+
+
+  // ALL PHASES ONLY BIDDER
+
+  function getBidderDeposit() external view onlyBidder returns (uint256) {
+    return bidderDeposit;
   }
 
-  function registerBidderAtFactory(address bidderAddress) private inSetup {
-    AuctionFactory auctionFactory = AuctionFactory(factory);
-    auctionFactory.registerBidder(bidderAddress);
+
+
+  // ALL PHASES ONLY SELLER OR BIDDER
+
+  function getPhase() external view onlySellerOrBidder returns (Phase) {
+    return phase;
   }
 
-  function inviteBidder(address bidderAddress) private inSetup {
-    require(!isInvitedBidder(bidderAddress), 'Bidder already exists');
-    bidders[bidderAddress].isInvited = true;
-    bidderAddresses.push(bidderAddress);
-    registerBidderAtFactory(bidderAddress);
-    emit LogBidderInvited(bidderAddress);
+  function getAsset() external view onlySellerOrBidder returns (uint256, address) {
+    return (tokenAmount, tokenContractAddress);
   }
-
-  function setupBidders(uint256 _bidderDeposit, address[] calldata _bidderAddresses) external onlySeller inSetup {
-    bidderDeposit = _bidderDeposit;
-    for (uint256 i = 0; i < _bidderAddresses.length; i++) {
-      inviteBidder(_bidderAddresses[i]);
-    }
-  }
-
-  function setWinner() internal {
-    address _winner = bidderAddresses[0];
-    for (uint256 i = 1; i < bidderAddresses.length; i++) {
-      address current = bidderAddresses[i];
-      if (bidders[current].bidHex > bidders[_winner].bidHex) {
-        _winner = current;
-      }
-    }
-    winner = _winner;
+  
+  function getWinner() external view onlySellerOrBidder returns (address, uint256) {
     uint256 winningBid = uint256(bidders[winner].bidHex);
-    emit LogSetWinner(winner, winningBid);
+    return (winner, winningBid);
   }
+
+
+
+  // ALL PHASES ONLY SELLER OR WINNER
+
+  function getEscrow() external view onlySellerOrWinner returns (Escrow) {
+    return escrow;
+  }
+
+
+
+  // PHASE CONTROL ONLY SELLER
 
   function startCommit() external onlySeller inSetup {
     phase = Phase.Commit;
-    // delayStart(oracle, jobId);
   }
 
   function startReveal() public onlySeller inCommit {
@@ -211,15 +196,38 @@ contract Auction is Initializable {
     phase = Phase.Withdraw;
   }
 
-  // function delayStart(address _oracle, bytes32 _jobId) private {
-  //   Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
-  //   req.addUint('until', now + 1 minutes);
-  //   sendChainlinkRequestTo(_oracle, req, oracleFee);
-  // }
 
-  // function fulfill(bytes32 _requestId) public recordChainlinkFulfillment(_requestId){
-  //   startReveal();
-  // }
+
+  // SETUP PHASE ONLY SELLER
+
+  function receiveSellerDeposit() external payable onlySeller inSetup {
+    sellerDeposit = msg.value;
+    emit LogSellerDepositReceived(msg.sender, msg.value);
+  }
+
+  function registerBidderAtFactory(address bidderAddress) private {
+    AuctionFactory auctionFactory = AuctionFactory(factory);
+    auctionFactory.registerBidder(bidderAddress);
+  }
+
+  function inviteBidder(address bidderAddress) private {
+    require(!isInvitedBidder(bidderAddress), 'Bidder already invited');
+    bidders[bidderAddress].isInvited = true;
+    bidderAddresses.push(bidderAddress);
+    registerBidderAtFactory(bidderAddress);
+    emit LogBidderInvited(bidderAddress);
+  }
+
+  function setupBidders(uint256 _bidderDeposit, address[] calldata _bidderAddresses) external onlySeller inSetup {
+    bidderDeposit = _bidderDeposit;
+    for (uint256 i = 0; i < _bidderAddresses.length; i++) {
+      inviteBidder(_bidderAddresses[i]);
+    }
+  }
+
+
+
+  // COMMIT PHASE ONLY BIDDER
 
   function receiveBidderDeposit() private {
     // consider using initialize or other modifier to prevent bidder from changing deposit
@@ -235,23 +243,38 @@ contract Auction is Initializable {
     emit LogBidCommitted(msg.sender, bidders[msg.sender].bidCommit, bidders[msg.sender].bidCommitBlock);
   }
 
-  function submitBid(bytes32 dataHash) external payable inCommit {
-    require(isInvitedBidder(msg.sender), 'Sender not authorized');
+  function submitBid(bytes32 dataHash) external payable onlyBidder inCommit {
     receiveBidderDeposit();
     commitBid(dataHash);
   }
 
-  function getSaltedHash(bytes32 data, bytes32 salt) public view returns (bytes32) {
-    return keccak256(abi.encodePacked(address(this), data, salt));
-  }
 
-  function revealBid(bytes32 bidHex, bytes32 salt) external inReveal {
-    require(isInvitedBidder(msg.sender), 'Sender not authorized');
+  // REVEAL PHASE ONLY BIDDER
+
+
+  function revealBid(bytes32 bidHex, bytes32 salt) external onlyBidder inReveal {
     require(bidders[msg.sender].isBidRevealed == false, 'Bid already revealed');
     require(getSaltedHash(bidHex, salt) == bidders[msg.sender].bidCommit, 'Revealed hash does not match');
     bidders[msg.sender].isBidRevealed = true;
     bidders[msg.sender].bidHex = bidHex;
     emit LogBidRevealed(msg.sender, bidHex, salt);
+  }
+
+
+
+  // DELIVER PHASE INTERNAL TRIGGERED BY PHASE CONTROL ONLY SELLER
+
+  function setWinner() internal {
+    address _winner = bidderAddresses[0];
+    for (uint256 i = 1; i < bidderAddresses.length; i++) {
+      address current = bidderAddresses[i];
+      if (bidders[current].bidHex > bidders[_winner].bidHex) {
+        _winner = current;
+      }
+    }
+    winner = _winner;
+    uint256 winningBid = uint256(bidders[winner].bidHex);
+    emit LogSetWinner(winner, winningBid);
   }
 
   function deployEscrow() internal {
@@ -260,7 +283,26 @@ contract Auction is Initializable {
     escrow.initialize(seller, winner, tokenAmount, tokenContractAddress, winningBid);
   }
 
-  function withdraw() external payable onlySeller {
-    seller.transfer(address(this).balance);
+
+
+  // WITHDRAW PHASE ONLY SELLER OR BIDDER
+
+  function withdrawSellerDeposit() external payable onlySeller inWithdraw {
+    // require(bothOk(), 'Escrow is not complete');
+    require(address(this).balance >= sellerDeposit, 'Insufficient balance');
+    // balance -= winningBid;
+    (bool success, ) = msg.sender.call.value(sellerDeposit)('');
+    require(success, 'Transfer failed');
+    emit LogSellerDepositWithdrawn(msg.sender, sellerDeposit);
   }
+
+  function withdrawBidderDeposit() external payable onlyBidder inWithdraw {
+    // require(bothOk(), 'Escrow is not complete');
+    require(address(this).balance >= bidderDeposit, 'Insufficient balance');
+    // balance -= winningBid;
+    (bool success, ) = msg.sender.call.value(sellerDeposit)('');
+    require(success, 'Transfer failed');
+    emit LogBidderDepositWithdrawn(msg.sender, bidderDeposit);
+  }
+
 }
