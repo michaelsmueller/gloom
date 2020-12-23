c6a097053fdf6a9957bfce94e5641d02b18f8c2a
Michael Mueller <hello@michaelmueller.dev>
2020-12-23 02:52:39 +0100

    replacing main with dev

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// const Auction = artifacts.require('./Auction.sol');
-const AuctionFactory = artifacts.require('./AuctionFactory.sol');
-
-module.exports = function (deployer) {
-  // deployer.deploy(Auction);
-  deployer.deploy(AuctionFactory);
-};
